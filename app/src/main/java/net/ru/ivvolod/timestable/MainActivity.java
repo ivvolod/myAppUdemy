e49159d47b2e3494fa29b45009d63ef8631d1b4c
Vladimir <ivannikov.volod@gmail.com>
2019-06-01 13:28:21 +0300

    add commit1

--- app/src/main/java/net/ru/ivvolod/timestable/MainActivity.java
+++ app/src/main/java/net/ru/ivvolod/timestable/MainActivity.java
@@ -3,12 +3,51 @@ package net.ru.ivvolod.timestable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listViewNumbers;
+    private SeekBar seekBar;
+    private ArrayList<Integer> numbers;
+    private int max = 20;
+    private int min = 1;
+    private int count = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listViewNumbers = findViewById(R.id.listViewNumbers);
+        seekBar = findViewById(R.id.seekBar);
+        /* Установка максимального значения у seekBar*/
+        seekBar.setMax(max);
+
+        numbers = new ArrayList<>();
+//      /*Добавление Адаптера для связки ArrayList с ListView*/
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1, numbers);
+        listViewNumbers.setAdapter(arrayAdapter);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                for (int i = min; i <count ; i++) {
+                    numbers.add(progress * i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
